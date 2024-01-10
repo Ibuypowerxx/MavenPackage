702bbd330a438350377fbd07e79e26ee360d069e
ibuypower <danroz1995@yandex.ru>
2024-01-10 12:00:23 +0300

    Correct

--- src/test/java/ru/netology/maven/mavenpackage/services/BonusServiceTest.java
+++ src/test/java/ru/netology/maven/mavenpackage/services/BonusServiceTest.java
@@ -8,12 +8,13 @@ public class BonusServiceTest {
     void shouldCalcthreemounth() {
         BonusService service = new BonusService();
 
-        int income = 1000;
+        int income = 10000;
         int expenses = 3000;
+        int threshold = 20000;
         long expected = 3;
 
         // вызываем целевой метод:
-        int actual = service.calculate(income, expenses);
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
@@ -26,10 +27,12 @@ public class BonusServiceTest {
 
         int income = 100_000;
         int expenses = 60_000;
+        int threshold = 150_000;
         long expected = 2;
 
+
         // вызываем целевой метод:
-        int actual = service.calculate(income, expenses);
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
